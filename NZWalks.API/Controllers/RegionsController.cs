f2551548f0692c5cad9b02723ee0c1fded91824a
rromerro900814094 <rromeror@indracompany.com>
2024-01-16 16:39:20 -0500

    Get all regions / Get region by id

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:port/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public RegionsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        // GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var region = dbContext.Regions.FirstOrDefault(r => r.Id == id);
+
+            if(region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+        }
+    }
+}

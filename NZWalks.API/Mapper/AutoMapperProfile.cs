57db7126bfd16cbf178efeb42377e544940769c7
rromerro900814094 <rromeror@indracompany.com>
2024-01-23 16:46:30 -0500

    Adding navigation properties

--- NZWalks.API/Mapper/AutoMapperProfile.cs
+++ NZWalks.API/Mapper/AutoMapperProfile.cs
@@ -13,6 +13,7 @@ namespace NZWalks.API.Mapper
             CreateMap<UpdateRegionRequestDto, Region>().ReverseMap();
             CreateMap<AddWalkRequestDto, Walk>().ReverseMap();
             CreateMap<Walk, WalkDto>().ReverseMap();
+            CreateMap<Difficulty, DifficultyDto>().ReverseMap();
         }
     }
 }

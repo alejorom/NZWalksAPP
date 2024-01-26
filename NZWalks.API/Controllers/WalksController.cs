9385f2c8f5f4cadb9115676d4ec3f8a63ad9615a
rromerro900814094 <rromeror@indracompany.com>
2024-01-26 09:58:46 -0500

    Adding Filtering

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -35,11 +35,11 @@ namespace NZWalks.API.Controllers
             return Ok(mapper.Map<WalkDto>(walkDomain));
         }
 
-        // GET: /api/walks
+        // GET: /api/walks?filterOn=Name&filterQuery=Track
         [HttpGet]
-        public async Task<IActionResult> GetAll ()
+        public async Task<IActionResult> GetAll ([FromQuery] string? filterOn, [FromQuery] string? filterQuery)
         {
-            var walksDomain = await walkRepository.GetAllAsync();
+            var walksDomain = await walkRepository.GetAllAsync(filterOn, filterQuery);
             
             // Map Domain Model to DTO
             return Ok(mapper.Map<List<WalkDto>>(walksDomain));
